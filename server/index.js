cd7adf4bdd275ec9e3f8e02e86c4f4a4bac4d1eb
hg-ian-choi <hg.ian.choi@gmail.com>
2022-10-23 00:56:12 +0900

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 5001
+const express = require('express');
+const app = express();
+const port = 5001;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb+srv://test:test@test.9qvtytm.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
